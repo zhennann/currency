241eb1880fe66fbf5c0ab5cf224d3193fe854b33
zhennann <zhen.nann@icloud.com>
2022-12-30 15:08:46 +0800

    chore: update

--- dist/index.js
+++ dist/index.js
@@ -36,20 +36,46 @@
 var __webpack_exports__ = {};
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "default": () => (/* binding */ debounce)
+/* harmony export */   "default": () => (/* binding */ Currency)
 /* harmony export */ });
-function debounce(func, wait) {
-  var i = 0;
-  return function () {
-    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
-      args[_key] = arguments[_key];
-    }
+function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
 
-    var ctx = this;
-    if (i !== 0) clearTimeout(i);
-    i = setTimeout(function () {
-      func.apply(ctx, args);
-    }, wait);
+function Currency(options) {
+  if (!options || _typeof(options) !== "object") {
+    options = {};
+  }
+
+  var fixed = options.fixed || 2;
+  var exp = options.exp || fixed;
+  var zero = options.zero || fixed;
+  return {
+    format: function format(value) {
+      if (isNaN(value)) return value;
+      var str = (Number(value) / Math.pow(10, exp)).toFixed(fixed);
+      return this._trimZero(str, zero);
+    },
+    update: function update(value) {
+      if (isNaN(value)) return value;
+      return Number((Number(value) * Math.pow(10, exp)).toFixed(0));
+    },
+    _trimZero: function _trimZero(str, zero) {
+      var indexZero = str.indexOf('.');
+      if (indexZero === -1) return str;
+
+      for (var index = str.length - 1; index > indexZero; index--) {
+        if (str[index] === '0' && index - indexZero > zero) {
+          str = str.substring(0, str.length - 1);
+        } else {
+          break;
+        }
+      }
+
+      if (indexZero === str.length - 1) {
+        str = str.substring(0, str.length - 1);
+      }
+
+      return str;
+    }
   };
 }
 module.exports = __webpack_exports__;
