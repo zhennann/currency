47b65ed04e132aa030036141ea90995f2e7b38a3
zhennann <zhen.nann@icloud.com>
2025-05-20 19:15:19 +0800

    feat: update

--- dist/index.js
+++ dist/index.js
@@ -1,37 +1,34 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Currency = void 0;
-var Currency = /** @class */ (function () {
-    function Currency(options) {
-        var _a, _b, _c;
+export class Currency {
+    fixed;
+    exp;
+    zero;
+    constructor(options) {
         if (options === false)
             throw new Error("Currency options cannot be false");
         if (!options || typeof options !== "object") {
             options = {};
         }
-        this.fixed = (_a = options.fixed) !== null && _a !== void 0 ? _a : 2;
-        this.exp = (_b = options.exp) !== null && _b !== void 0 ? _b : this.fixed;
-        this.zero = (_c = options.zero) !== null && _c !== void 0 ? _c : this.fixed;
+        this.fixed = options.fixed ?? 2;
+        this.exp = options.exp ?? this.fixed;
+        this.zero = options.zero ?? this.fixed;
     }
-    Currency.prototype.format = function (value) {
+    format(value) {
         if (isNaN(value))
             return String(value);
-        var str = (Number(value) / Math.pow(10, this.exp)).toFixed(this.fixed);
+        const str = (Number(value) / Math.pow(10, this.exp)).toFixed(this.fixed);
         return _trimZero(str, this.zero);
-    };
-    Currency.prototype.update = function (value) {
+    }
+    update(value) {
         if (isNaN(value))
             return value;
         return Number((Number(value) * Math.pow(10, this.exp)).toFixed(0));
-    };
-    return Currency;
-}());
-exports.Currency = Currency;
+    }
+}
 function _trimZero(str, zero) {
-    var indexZero = str.indexOf(".");
+    let indexZero = str.indexOf(".");
     if (indexZero === -1)
         return str;
-    for (var index = str.length - 1; index > indexZero; index--) {
+    for (let index = str.length - 1; index > indexZero; index--) {
         if (str[index] === "0" && index - indexZero > zero) {
             str = str.substring(0, str.length - 1);
         }
@@ -44,3 +41,4 @@ function _trimZero(str, zero) {
     }
     return str;
 }
+//# sourceMappingURL=index.js.map
